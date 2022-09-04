e5f1122cafd4d6753320e54c564dff8281a840d7
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2022-09-05 00:00:20 +0800

    🌱 Move the theme to a separate repo

--- assets/js/component/dropdown.js
+++
@@ -1,6 +0,0 @@
-let dropdowns = document.querySelectorAll('.dropdown')
-dropdowns.forEach(el => {
-    el.addEventListener('click', function () {
-        el.querySelector('.dropdown-menu').classList.toggle('show');
-    })
-});
